028df95b82841ee71d955c0db62c8c5d24e0154f
sarah arthur <arthursarah759@gmail.com>
2019-07-22 10:24:04 +0100

    cuties

---
+++ globalcode.py
@@ -0,0 +1,75 @@
+Project kick off
+Here are some things to consider while deciding on your final week project:
+
+Your product
+What are you going to make?
+
+What problem does this solve?
+
+Who are you making it for?
+
+What technologies / programming languages are you going to use?
+
+Your planning
+How are you going to divide up the work?
+
+How are you going to plan your time?
+
+How are you going to keep track of your progress?
+
+How are you going to support your team mates?
+
+What might you need to research?
+
+What other help might you need?
+
+
+OUR PROJECT:
+
+Weather sensor
+
+It helps the user to know the weather condition in order to avoid  weather accident(either a rainfall or hot sun shine) or embarrassment
+
+Personal use(It will be used by a single individual)
+
+GPIO and Pthon language
+
+OUR PLANNING:
+
+Our projects will be divided into three main parts:
+
+LED and wire connectivity on the raspberry pi
+
+Coding and creating files for projects
+
+Repository work done
+
+TIME MANAGEMENT:
+
+9am to 12pm -
+coding
+researches
+group suggestions and discussions to bring ideas for the project
+
+1pm to 3pm-
+LED and wire connectivity
+Running of program code
+
+3pm to 4pm
+Check-in
+Packing
+
+TRACKING OF PROGRESS:
+With the use of Trello app
+
+RESPONSIBILITIES:
+By playing your part diligently
+Being available when needed
+
+RESEARCH MATERIALS:
+Internet connection
+
+OTHER HELP:
+Help from volunteers
+Provision of weather sensor (BME280 wearher sensor if possible)
+
